11af13bf923721b9f0d54411256e94cf1e49c09c
Kwaku Sarfo Kissi <kwakusarfokissi.ksk@gmail.com>
2024-06-20 14:47:09 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,7 +3,7 @@
  * main - A program that prints the size of various computer types
  * Return 0 (Success)
  */
-int main(vod)
+int main(void)
 {
 	char a;
 	int b;
@@ -15,6 +15,6 @@ printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
 printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(b));
 printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c));
 printf("Size of a long long int: %lu  byte(s)\n", (unsigned long)sizeof(d));
-print("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
+printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
 return (0);
 }
