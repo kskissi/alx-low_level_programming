937e72a996844a925c09e49c0346de030161df20
Kwaku Sarfo Kissi <kwakusarfokissi.ksk@gmail.com>
2024-06-20 15:43:28 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Ac program that prints a line to the standard error
+ * Return 1 (Successs)
+ */
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return (1);
+}
