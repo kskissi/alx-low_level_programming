59299c71e685df8916a2762ae596b4fd10d4bf5c
Kwaku Sarfo Kissi <kwakusarfokissi.ksk@gmail.com>
2024-06-20 16:12:44 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,13 +2,13 @@
 
 /**
  * main - prints "with proper grammar, but the outcome is a piece of art," followed newline
- * Return: Always 0
+ * Return: Always 0.
  *
  */
 
 int main(void)
 
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n"):
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
