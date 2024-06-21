d31eaf81268b1d33fc902b0fc44de73c74e24a91
Kwaku Sarfo Kissi <kwakusarfokissi.ksk@gmail.com>
2024-06-21 16:13:05 -0700

    Numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,24 +1,22 @@
 #include <stdio.h>
 
-
 /**
- * main - program that prints the alphabet in lowercase and upper case
+ * main - program that prints the digits from 0 to 9
  *
  * Return: Always 0.
  */
 
 int main(void)
 {
-	char letter;
+    char digit;
 
-	for (letter = 'a'; letter <= 'z'; letter++)
+    for (digit = '0'; digit <= '9'; digit++)
+    {
+        putchar(digit);
+    }
 
-	{
-	if (letter != 'e' && letter != 'q')
-	putchar(letter);
-	}
+    putchar('\n');
 
-	putchar('\n');
-
-	return (0);
+    return (0);
 }
+
