72f51beaaa438cd20c1c806a169b2e94fb2544d5
Kwaku Sarfo Kissi <kwakusarfokissi.ksk@gmail.com>
2024-06-23 09:11:59 -0700

    Hello print f

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,9 @@
 #include <stdio.h>
-
 /**
- * main - prints "with proper grammar, but the outcome is a piece of art," followed newline
- * Return: Always 0.
- *
+ * main - this is a prinf function
+ * Return: 0
  */
-
 int main(void)
-
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
