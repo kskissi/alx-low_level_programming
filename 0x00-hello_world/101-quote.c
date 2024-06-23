236925ab1178fcb5057e892f87f52e1b77355b6b
Kwaku Sarfo Kissi <kwakusarfokissi.ksk@gmail.com>
2024-06-23 09:17:40 -0700

    Unix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,11 @@
-include <unistd.h>
 
-/**main - prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
- * followed by a newline, to standard error.
- * Return: Always 1.
+101	#include <unistd.h>
+/**
+ 	* main - this prints exactly the input on it
+ * Return: always 1.
  */
-
-
 int main(void)
-
 {
-	wtite(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
+}
