dafdc3642def14340866b179583af481cc403189
Kwaku Sarfo Kissi <kwakusarfokissi.ksk@gmail.com>
2024-06-25 13:09:06 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -4,24 +4,24 @@
  * print_alphabet_x10 - Prints the alphabet 10 times
  *
  * Description:
- * This function prints all lowercase letters from 'a' to 'z' followed by a newline,
- * and it repeats this process 10 times.
+ * This function prints all lowercase letters from 'a' to 'z' followed by
+ * a newline, and it repeats this process 10 times.
  *
  * Return: void (no return value)
  */
 void print_alphabet_x10(void)
 {
-    char c;
-    int i = 0;
+	char c;
+	int i = 0;
 
-    while (i <= 9)
-    {
-        for (c = 'a'; c <= 'z'; c++)
-        {
-            _putchar(c);
-        }
-        _putchar('\n');
-        i++;
-    }
+	while (i <= 9)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
+		i++;
+	}
 }
 
