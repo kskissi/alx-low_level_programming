731da8740166075b9c4312cf669ea01cc6d2c02b
Kwaku Sarfo Kissi <kwakusarfokissi.ksk@gmail.com>
2024-06-25 21:47:24 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -2,58 +2,45 @@
 
 /**
  * print_times_table - Prints the times table of the input, starting with 0.
- * @n: The value of the times table to be printed (0 <= n <= 15).
+ * @n: The value of the times table to be printed.
  */
 void print_times_table(int n)
 {
-	if (n < 0 || n > 15)
-		return;
-
 	int num, mult, prod;
 
-	for (num = 0; num <= n; num++)
+	if (n >= 0 && n <= 15)
 	{
-		_putchar('0');
-		for (mult = 1; mult <= n; mult++)
+		for (num = 0; num <= n; num++)
 		{
-			prod = num * mult;
-			print_number(prod, mult, n);
+			_putchar('0');
+			for (mult = 1; mult <= n; mult++)
+			{
+				_putchar(',');
+				_putchar(' ');
+
+				prod = num * mult;
+
+				if (prod <= 9)
+					_putchar(' ');
+				if (prod <= 99)
+					_putchar(' ');
+
+				if (prod >= 100)
+				{
+					_putchar((prod / 100) + '0');
+					_putchar(((prod / 10)) % 10 + '0');
+				}
+				else if (prod >= 10 || mult == 1)
+				{
+					_putchar(' ');
+					if (prod >= 10)
+						_putchar((prod / 10) + '0');
+				}
+
+				_putchar((prod % 10) + '0');
+			}
+			_putchar('\n');
 		}
-		_putchar('\n');
 	}
-}
-
-/**
- * print_number - Helper function to print a formatted number.
- * @prod: The product to be printed.
- * @mult: The multiplier value.
- * @max: The maximum value of n for formatting purposes.
- */
-void print_number(int prod, int mult, int max)
-{
-	_putchar(',');
-	_putchar(' ');
-
-	if (prod <= 9)
-	{
-		_putchar(' ');
-		_putchar(' ');
-	}
-	else if (prod <= 99)
-	{
-		_putchar(' ');
-	}
-
-	if (prod >= 100)
-	{
-		_putchar((prod / 100) + '0');
-		_putchar(((prod / 10) % 10) + '0');
-	}
-	else if (prod >= 10 || mult == 1)
-	{
-		_putchar(' ');
-	}
-
-	_putchar((prod % 10) + '0');
 }
 
