531012a4b87c19ec7e55f24415b10bcc2c39b856
Kwaku Sarfo Kissi <kwakusarfokissi.ksk@gmail.com>
2024-06-26 23:33:32 -0700

    1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,7 +1,25 @@
-arrie@ubuntu:/debugging$ gcc -std=gnu89 1-main.c -o 1-main
-carrie@ubuntu:/debugging$ ./1-main
-Infinite loop incoming :(
-Infinite loop avoided! \o/
-carrie@ubuntu:/debugging$ wc -l 1-main.c
-24 1-main.c
-carrie@ubuntu:/debugging$
+#include <stdio.h>
+
+/**
+ * main - causes an infinite loop
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	printf("Infinite loop incoming :(\n");
+
+	i = 0;
+	/*
+	 * while (i < 10)
+	 * {
+	 *     putchar(i);
+	 * }
+	 */
+	printf("Infinite loop avoided! \\o/\n");
+
+	return (0);
+}
+
