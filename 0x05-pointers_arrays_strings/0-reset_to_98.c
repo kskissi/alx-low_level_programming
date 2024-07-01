87b48188cef67c7a82c5fcf3d65107214afec598
Kwaku Sarfo Kissi <kwakusarfokissi.ksk@gmail.com>
2024-07-01 13:15:05 -0700

    0-reset_to_98

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,15 +2,13 @@
 
 /**
  * reset_to_98 - Takes a pointer to an int and updates
- * the value it points to to 98
- *
+ *               the value it points to to 98
  * @n: An integer input pointer
  *
- * Return : nothing
+ * Return: nothing
  */
 void reset_to_98(int *n)
 {
-
-	*n = 98;
-
+*n = 98;
 }
+
