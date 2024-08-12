1faf0114e5c11f2a9cf7987c921c8a5f888aaad4
kskissi <kwakusarfokissi.ksk@gmail.com>
2024-08-12 05:31:45 -0700

    0-read_textfile.c

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,43 @@
+#include "main.h"
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+/**
+ * read_textfile - Read text file and print to STDOUT.
+ * @filename: The text file to be read.
+ * @letters: The number of letters to read.
+ * 
+ * Return: The actual number of bytes read and printed, or
+ *         0 if the function fails or filename is NULL.
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+    char *buf;
+    ssize_t fd;
+    ssize_t w;
+    ssize_t t;
+
+    if (filename == NULL)
+        return (0);
+
+    fd = open(filename, O_RDONLY);
+    if (fd == -1)
+        return (0);
+
+    buf = malloc(sizeof(char) * letters);
+    if (buf == NULL)
+    {
+        close(fd);
+        return (0);
+    }
+
+    t = read(fd, buf, letters);
+    w = write(STDOUT_FILENO, buf, t);
+
+    free(buf);
+    close(fd);
+
+    return (w);
+}
+
