2215ba4ee547b5c2f4678dec09f41620b34a2f6c
kskissi <kwakusarfokissi.ksk@gmail.com>
2024-08-12 05:34:21 -0700

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,43 +1,49 @@
 #include "main.h"
 #include <stdlib.h>
-#include <unistd.h>
 #include <fcntl.h>
+#include <unistd.h>
 
 /**
  * read_textfile - Read text file and print to STDOUT.
- * @filename: The text file to be read.
- * @letters: The number of letters to read.
- * 
- * Return: The actual number of bytes read and printed, or
- *         0 if the function fails or filename is NULL.
+ * @filename: text file being read
+ * @letters: number of letters to be read
+ *
+ * Return: actual number of bytes read and printed,
+ *         or 0 if function fails or filename is NULL.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-    char *buf;
-    ssize_t fd;
-    ssize_t w;
-    ssize_t t;
+	char *buf;
+	ssize_t fd;
+	ssize_t w;
+	ssize_t t;
 
-    if (filename == NULL)
-        return (0);
+	if (filename == NULL)
+		return (0);
 
-    fd = open(filename, O_RDONLY);
-    if (fd == -1)
-        return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
 
-    buf = malloc(sizeof(char) * letters);
-    if (buf == NULL)
-    {
-        close(fd);
-        return (0);
-    }
+	buf = malloc(sizeof(char) * letters);
+	if (buf == NULL)
+	{
+		close(fd);
+		return (0);
+	}
 
-    t = read(fd, buf, letters);
-    w = write(STDOUT_FILENO, buf, t);
+	t = read(fd, buf, letters);
+	if (t == -1)
+	{
+		free(buf);
+		close(fd);
+		return (0);
+	}
 
-    free(buf);
-    close(fd);
+	w = write(STDOUT_FILENO, buf, t);
 
-    return (w);
+	free(buf);
+	close(fd);
+	return (w);
 }
 
